497b3d0ee2456c676108d31504c51b110786c4e4
Ian <ian.esling@gmail.com>
2014-10-30 14:21:56 +0000

    variable length string generator

--- src/fyodor_clj/core.clj
+++ src/fyodor_clj/core.clj
@@ -1,14 +1,21 @@
 (ns fyodor-clj.core
-  (import (uk.org.fyodor.generators RDG)))
+  (import (uk.org.fyodor.generators RDG)
+          (uk.org.fyodor.range Range)))
 
-(def default-string-length 30)
+(def default-string-length (int  30))
 
-(defn string
-  "Returns a function that generates random strings."
-  ([] (string (int default-string-length)))
+(defn string-fixed-length
+  "Returns a function that generates random strings of fixed length."
+  ([] (string-fixed-length (int default-string-length)))
   ([x]
      (def gen (RDG/string (int x)))
      (fn [] (.next gen)))
   ([x chars]
      (def gen (RDG/string (int x) (str chars)))
      (fn [] (.next gen))))
+
+(defn string-variable-length
+  "returns a function that generates random strings of variable length."
+  [min max]
+  (def gen (RDG/string (Range/closed (int min) (int max))))
+  (fn [] (.next gen)))
--- test/fyodor_clj/core_test.clj
+++ test/fyodor_clj/core_test.clj
@@ -2,12 +2,19 @@
   (:require [clojure.test :refer :all]
             [fyodor-clj.core :refer :all]))
 
-(deftest string-generator
+(deftest string-fixed-length-generator
   (testing "string function returns a string of the right length."
-    (is (string? ((string))))
-    (is (= 15 (count ((string 15)))))
-    (is (= 12 (count ((string 12)))))
-    (is (= 42 (count ((string 42))))))
+    (is (string? ((string-fixed-length))))
+    (is (= 15 (count ((string-fixed-length 15)))))
+    (is (= 12 (count ((string-fixed-length 12)))))
+    (is (= 42 (count ((string-fixed-length 42))))))
   (testing "string generator uses provided characters"
-    (is (.matches ((string 10 "ABC")) "[ABC]{10}" ))
-    (is (.matches ((string 42 "XYZ")) "[XYZ]{42}"))))
+    (is (.matches ((string-fixed-length 10 "ABC")) "[ABC]{10}" ))
+    (is (.matches ((string-fixed-length 42 "XYZ")) "[XYZ]{42}"))))
+
+(deftest string-variable-length-generator
+  (testing "generate strings of variable length"
+    (is (string? ((string-variable-length 10 20))))
+    (is (>= (count ((string-variable-length 10 20))) 10))
+    (is (<= (count ((string-variable-length 10 20))) 20))
+    ))
